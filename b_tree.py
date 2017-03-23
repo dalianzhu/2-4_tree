5cf7078d7bc44610af3bb610dadec7e4044aa9bb
dalianzhu <yinzihao1@126.com>
2017-03-23 15:29:13 +0800

    完成B树删除功能 增加新的测试用例 	modified:   b_tree.py 	modified:   test.py

--- b_tree.py
+++ b_tree.py
@@ -2,6 +2,8 @@ CONST_MAX_KEY = 4
 CONST_MIN_KEY = 2
 
 isdebug = True
+
+
 def debug(str):
     if isdebug:
         print(str)
@@ -16,6 +18,7 @@ class BTree(object):
     B树的每个节点包含的KEYS 数量都在2-4之间。
     添加节点从叶子节点开始。如果叶子节点的数量大于4,将会发生分裂。
     """
+
     def __init__(self):
         self.root = None
 
@@ -41,8 +44,141 @@ class BTree(object):
                 if len(node.keys) > CONST_MAX_KEY:
                     self.split(node)
 
-    def remove(self,key):
-        pass
+    def remove(self, key):
+        """
+        :param key: 要删除的值
+        :return:
+        """
+        # 首先 定位到这个值可以出现的节点
+        target_node = self.find(key)
+        self._remove(target_node, key)
+
+    def _remove(self, delnode, key):
+        """
+        从delnode中，删除key
+        :param delnode:当前要被删除的Node
+        :param key:当前要被删除的值，即从 node中删除该值
+        """
+        if key not in delnode.keys:
+            return
+
+        if not delnode.has_children():
+            # 说明这是一个叶子
+            delnode.keys.remove(key)
+            delnode.children.remove(None)  # 叶子节点的children都是none,删谁都一样
+
+            if len(delnode.keys) < CONST_MIN_KEY:
+                # keys数量合规，这种情况不需要再平衡
+                self._rebalance(delnode)
+        else:
+            # 说明在中间节点删除
+            # 需要将最近子树的节点上移
+            # 然后改为删除子树的这个节点
+            current_key_pos = delnode.find_key_position(key)
+            left_node = delnode.children[current_key_pos]
+            delnode.keys[current_key_pos], left_node.keys[len(left_node.keys) - 1] = \
+                left_node.keys[len(left_node.keys) - 1], delnode.keys[current_key_pos]
+
+            delnode = left_node
+            self._remove(delnode, key)
+
+    def _rebalance(self, delnode):
+        if self.root == delnode:
+            # merge根节点
+            if len(delnode.keys) == 0:
+                self.root = self.root.children[0]
+            return
+
+        # 说明删除的是叶子 or 树干节点，进行合并操作
+        # 首先把delnode和父节点与合适的兄弟合并成一个节点
+        new_node, old_parent_key = self._new_node_with_parent_fat_brother(delnode)
+        parent = delnode.parent
+        old_parent_key_pos = parent.find_key_position(old_parent_key)
+
+        # 判断，new_node的Keys 如果 > CONST_MAX_KEY，说明需要马上分裂
+        # 此情况下不会影响父以上的结构，因为不需要从父借节点，而是把子节点中的某个key替换给父
+        # 分裂后，生成新的节点，和左右子树
+        if len(new_node.keys) > CONST_MAX_KEY:
+            mid_key, left, right = new_node.split()
+            # 把新的mid key对父节点的目标key赋值
+            # 绑定左右子树
+            parent.keys[old_parent_key_pos] = mid_key
+
+            parent.children[old_parent_key_pos] = left
+            left.parent = parent
+            parent.children[old_parent_key_pos + 1] = right
+            right.parent = parent
+
+        # 如果构造的新节点，刚好等于 CONST_MAX_KEY
+        # 在此情况下，不能分裂，所以从父节点借来的key，不能归还
+        if len(new_node.keys) == CONST_MAX_KEY:
+            # 父节点对应的keys删除，返回被删除的位置
+            del_pos = parent.del_key(old_parent_key)
+
+            # 被删除的位置恰好是父节点对应子树的位置
+            parent.children[del_pos] = new_node
+            new_node.parent = parent
+
+        # 父节点可能发生了变化，如果被借了节点，且借后节点小于2个递归处理
+        if len(parent.keys) < CONST_MIN_KEY:
+            self._rebalance(parent)
+
+    def _new_node_with_parent_fat_brother(self, input_node):
+        """
+        传入一个节点，让它和父亲的合适KEY及最胖的兄弟构成一个新节点
+        :param input_node:传入的节点
+        :return:新节点，原来父节点的合适key
+        """
+        parent = input_node.parent
+        this_pos = parent.find_child_position(input_node)  # 首先找到delnode的位置
+
+        left_brother_pos = this_pos - 1
+        right_brother_pos = this_pos + 1
+        # 得到两个兄弟（不一定两个都存在，但必有一个），我们选择一个比较胖的兄弟作为待合并兄弟
+        fatter_brother_pos = self._choose_fat_child(parent, left_brother_pos, right_brother_pos)
+
+        if fatter_brother_pos == left_brother_pos:
+            #        parent
+            #        /   \
+            #      left   input_node
+            #   会生成新节点：
+            #   left_parent_inputnode
+
+            new_node = BTreeNode()
+            left_node_keys = parent.children[left_brother_pos].keys
+            parent_key_val = parent.keys[this_pos - 1]
+            input_node_keys = input_node.keys
+            new_node.keys = left_node_keys + [parent_key_val] + input_node_keys
+            new_node.children = parent.children[left_brother_pos].children + input_node.children
+        else:
+            # 说明
+            #        parent
+            #        /       \
+            #      input_node   right
+            #   会生成新节点：
+            #   inputnode_parent_right
+
+            new_node = BTreeNode()
+
+            input_node_keys = input_node.keys
+            parent_key_val = parent.keys[this_pos]
+            right_node_keys = parent.children[right_brother_pos].keys
+
+            new_node.keys = input_node_keys + [parent_key_val] + right_node_keys
+            new_node.children = parent.children[left_brother_pos].children + input_node.children
+
+        return new_node, parent_key_val
+
+    def _choose_fat_child(self, current_node, child1_pos, child2_pos):
+        positions = [child1_pos, child2_pos]
+        positions = [x for x in positions if x in range(0, len(current_node.children))]
+
+        if not positions:
+            return None
+
+        positions.sort(key=lambda x: len(current_node.children[x].keys))
+
+        return positions[len(positions) - 1]
 
     def find(self, key):
         return self._find(self.root, key)
@@ -61,23 +197,8 @@ class BTree(object):
                 return node
 
     def split(self, node):
-        # 分裂开始，首先生成新的左右子树节点
-        left = BTreeNode()
-        right = BTreeNode()
-
-        # 找到分裂的位置，如果keys == 5，则中间的分裂点为 3
-        mid_position = len(node.keys) // 2
-        # 求分裂的中心值，这个中心值将被加到父节点中去
-        mid_key = node.keys[mid_position]
-
-        # 处理左子树
-        left.keys = [v for k, v in enumerate(node.keys) if k < mid_position]
-        left.children = [v for k, v in enumerate(node.children) if k <= mid_position]
-
-        # 处理右子树
-        right.keys = [v for k, v in enumerate(node.keys) if k > mid_position]
-        right.children = [v for k, v in enumerate(node.children) if k > mid_position]
-
+        # 首先，分裂一个节点
+        mid_key, left, right = node.split()
         # 把中间值加到父节点中
         parent = node.parent
         if parent:
@@ -109,11 +230,10 @@ class BTree(object):
             right.parent = self.root
 
 
-
 class BTreeNode(object):
     def __init__(self):
         self.keys = []
-        self.children = [None] # children len == len(key) + 1
+        self.children = [None]  # children len == len(key) + 1
         self.parent = None
 
     def add_key(self, key):
@@ -122,7 +242,16 @@ class BTreeNode(object):
         self.children.insert(pos + 1, None)  # 插入了一个key，children当然也要相应变化
         return pos
 
-    def find_sub_position(self, key):
+    def del_key(self, key):
+        pos = self.find_key_position(key)
+        if pos == -1:
+            return pos
+
+        self.keys.pop(pos)
+        self.children.pop(pos)
+        return pos
+
+    def find_sub_position(self, subkey):
         """
         根据Key查找子树的位置，如
         arr = [3,5,   9,12]
@@ -136,11 +265,23 @@ class BTreeNode(object):
 
         position = 0
         for k, v in enumerate(self.keys):
-            if key > v:
+            if subkey > v:
                 position += 1
             else:
                 break
         return position
+
+    def find_key_position(self, key):
+        for k, v in enumerate(self.keys):
+            if key == v:
+                return k
+        return -1
+
+    def find_child_position(self, child_node):
+        for k, v in enumerate(self.children):
+            if v == child_node:
+                return k
+        return -1
 
     def has_children(self):
         for item in self.children:
@@ -148,7 +289,22 @@ class BTreeNode(object):
                 return True
         return False
 
+    def split(self):
+        # 分裂开始，首先生成新的左右子树节点
+        left = BTreeNode()
+        right = BTreeNode()
 
+        # 找到分裂的位置，如果keys == 5，则中间的分裂点为 3
+        mid_position = len(self.keys) // 2
+        # 求分裂的中心值，这个中心值将被加到父节点中去
+        mid_key = self.keys[mid_position]
 
+        # 处理左子树
+        left.keys = [v for k, v in enumerate(self.keys) if k < mid_position]
+        left.children = [v for k, v in enumerate(self.children) if k <= mid_position]
 
+        # 处理右子树
+        right.keys = [v for k, v in enumerate(self.keys) if k > mid_position]
+        right.children = [v for k, v in enumerate(self.children) if k > mid_position]
 
+        return mid_key, left, right
--- test.py
+++ test.py
@@ -12,13 +12,118 @@ def test_case_1():
     tree.add(8)
     tree.add(14)
     tree.add(17)
-    debug("tree root key {}".format(tree.root.keys))
+
     assert tree.root.keys == [4, 12]
     assert tree.root.children[1].keys == [8, 9]
 
 
+def test_case_2():
+    """
+    建立一个树
+                  5
+               /    \
+            2,3    6,7,8
+    删除8，不会对结构有影响
+    """
+    debug("test case 2 run")
+    keys = [5, 2, 3, 6, 7, 8]
+
+    tree = BTree()
+    for item in keys:
+        tree.add(item)
+
+    tree.remove(8)
+    assert tree.root.keys == [5]
+    assert tree.root.children[0].keys == [2, 3]
+    assert tree.root.children[1].keys == [6, 7]
+
+
+def test_case_3():
+    """
+    建立一个树
+                  5
+               /    \
+            2,3    6,7,8
+    删除3
+    结果需要再平衡
+              6
+            /   \
+         2,5    7,8
+    """
+    debug("test case 3 run")
+    keys = [5, 2, 3, 6, 7, 8]
+
+    tree = BTree()
+    for item in keys:
+        tree.add(item)
+
+    tree.remove(3)
+    assert tree.root.keys == [6]
+    assert tree.root.children[0].keys == [2, 5]
+    assert tree.root.children[1].keys == [7, 8]
+
+
+def test_case_4():
+    """
+    建立一个树：
+                             5       10      44      55
+                      |      |        |       |      |        |
+                        /        /        /       /        /
+                   2 3         6 7      17 22   45 50    66 68 70
+    删除 3
+
+                            10       44      55
+                      |      |        |       |      |
+                        /         /       /        /
+                   2 5 6 7       17 22   45 50    66 68 70
+
+    删除 44
+    先替换
+                            10       22      55
+                      |      |        |       |      |
+                        /         /       /        /
+                   2 5 6 7     17 *44*   45 50    66 68 70
+
+    再合并最胖兄弟
+                             7       22      55
+                      |      |        |       |      |
+                        /         /       /        /
+                   2 5 6      10 17    45 50    66 68 70
+    """
+
+    debug("test case 4 run")
+    keys = [5, 10, 50, 2, 3, 6, 7, 17, 22, 44, 45, 55, 66, 68, 70]
+
+    tree = BTree()
+    for item in keys:
+        tree.add(item)
+
+    tree.remove(3)
+    assert tree.root.keys == [10, 44, 55]
+    assert tree.root.children[0].keys == [2, 5, 6, 7]
+    assert tree.root.children[1].keys == [17, 22]
+
+    tree.remove(44)
+    assert [
+               tree.root.keys,
+               tree.root.children[0].keys,
+               tree.root.children[1].keys,
+               tree.root.children[2].keys,
+               tree.root.children[3].keys,
+           ] == [
+               [7, 22, 55],
+               [2, 5, 6],
+               [10, 17],
+               [45, 50],
+               [66, 68, 70],
+           ]
+
+
 test_cases = [
     test_case_1,
+    test_case_2,
+    test_case_3,
+    test_case_4,
 ]
 for item in test_cases:
     item()
